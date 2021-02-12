20f3d66f3aeef5dbaafdb07ec40fdccab0e16fda
Hamberfim <adhamlingithub@gmail.com>
2021-02-12 17:46:45 -0600

    clean up presentation

--- stringJoinComparing/Program.cs
+++ stringJoinComparing/Program.cs
@@ -19,47 +19,47 @@ namespace stringConcatJoinCompare
             Console.Title = "Strings and some String Methods";
 
             string[] a = new string[3] {"Alpha", "Beta", "Gamma"};
-            Console.WriteLine("This is the string array: " + a[0] + ", " + a[1] + ", " + a[2]);
+            Console.WriteLine("  ===== This is the string array =====  \n\tstring[] a = new string[3] {" + a[0] + ", " + a[1] + ", " + a[2] + "}");
             Console.WriteLine();  // new line - because I hate using \n ...it's just looks sloppy.
 
             // concatenation
-            Console.WriteLine("==== Concatenation using String.Concat ===");
+            Console.WriteLine("  ===== Concatenation using String.Concat =====");
             string s = String.Concat(a[0], a[1]);
-            Console.WriteLine("Concat a[0], a[1]: " + s);
+            Console.WriteLine("\tConcat a[0], a[1]: " + s);
             Console.WriteLine();  // new line - because I hate using \n ...it's just looks sloppy.
 
             // join with a declarded seperator
-            Console.WriteLine("==== Concatenation using String.Join ===");
+            Console.WriteLine("  ===== Concatenation using String.Join =====");
             s = String.Join("-", a[0], a[1]);
-            Console.WriteLine("Join a[0], a[1] with a seperator '-': " + s);
+            Console.WriteLine("\tJoin a[0], a[1] with a seperator '-': " + s);
 
             // join with a declarded seperator
             s = String.Join("<br>", a[0], a[1]);
-            Console.WriteLine("Join a[0], a[1] with an html tag: " + s);
+            Console.WriteLine("\tJoin a[0], a[1] with an html tag: " + s);
             Console.WriteLine();  // new line - because I hate using \n ...it's just looks sloppy.
 
-            Console.WriteLine("==== Comparing using String.Compare ======");
+            Console.WriteLine("  ===== Comparing using String.Compare =====");
             int num = String.Compare(a[0], a[1]);
-            Console.WriteLine(a[0] + " v " + a[1] + ":\t" + num);
+            Console.WriteLine("\t" + a[0] + " v " + a[1] + ":\t" + num);
 
             num = String.Compare(a[2], a[1]);
-            Console.WriteLine(a[2] + " v " + a[1] + ":\t" + num);
+            Console.WriteLine("\t" + a[2] + " v " + a[1] + ":\t" + num);
             Console.WriteLine();  // new line - because I hate using \n ...it's just looks sloppy.
 
-            Console.WriteLine("==== Comparing using .CompareTo ======");
+            Console.WriteLine("  ===== Comparing using .CompareTo =====");
             num = a[1].CompareTo(a[1]);
-            Console.WriteLine(a[1] + " v " + a[1] + ":\t" + num);
+            Console.WriteLine("\t" + a[1] + " v " + a[1] + ":\t" + num);
             Console.WriteLine();  // new line - because I hate using \n ...it's just looks sloppy.
 
             // Boolean comparison
-            Console.WriteLine("==== Comparing using boolean/.Equals ======");
+            Console.WriteLine("  ===== Comparing using boolean/.Equals =====");
             bool flag = (a[0] == a[1]);
-            Console.WriteLine(a[0] + " == " + a[1] + ":\t" + flag);
+            Console.WriteLine("\t" + a[0] + " == " + a[1] + ":\t" + flag);
 
             flag = a[2].Equals(a[2]);
-            Console.WriteLine(a[2] + " == " + a[2] + ":\t" + flag);
+            Console.WriteLine("\t" + a[2] + " == " + a[2] + ":\t" + flag);
 
-            Console.WriteLine("\n\n\n");  // push exit message down in console
+            Console.WriteLine("\n\n");  // push exit message down in console
         }
     }
 }
