6a09faa8a9b1ff257c2aa2fbb9311f5bd3d4a569
Hamberfim <adhamlingithub@gmail.com>
2021-02-15 18:27:08 -0600

    finished ToCopy, Insert, Replace etc.

--- stringCopySwap/Program.cs
+++ stringCopySwap/Program.cs
@@ -27,6 +27,36 @@ namespace stringCopySwap
                 Console.Write(c + " ");
             }
 
+            Console.WriteLine();  // need a new line
+
+            car1 = car1.Remove(5);
+            Console.WriteLine("Removed...\tCar1: " + car1);
+
+            car1 = car1.Insert(0, "Maserati ");  // insert at beginning
+            car1 = car1.Insert(14, " Cabrio");  // insert at end
+            Console.WriteLine("Inserted...\tCar1: " + car1);
+
+            car1 = car1.Replace("Grand Cabrio", "Quattroporte");  // must match
+            Console.WriteLine("Replaced...\tCar1: " + car1);
+
+            car1 = car1.Replace("Maserati Quattroporte", "Rusty POS!");
+            Console.WriteLine("Replaced...\tCar1: " + car1 + " is worse than a " + car2);
+
+            Console.WriteLine();  // need a new line
+            Console.WriteLine();  // space
+
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            int charArrayLength = name.Length;  // get the length of the input name
+            char[] nameArray = new char[charArrayLength];  // create array same length as input name
+            name.CopyTo(0, nameArray, 0, charArrayLength);  // copy the name into the array
+            Console.WriteLine("You entered: " + name + ", the system sees it as a " + name.GetType() + " data type.");
+            Console.WriteLine("A character array of your name:");
+            foreach(char c in nameArray)
+            {
+                Console.Write(c + " ");  // output the each cahr in the array on a line
+            }
 
 
             Console.WriteLine("\n\n\n");
