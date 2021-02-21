548d5eef0e2187a6d9d46a52412199dc5e39aec1
Hamberfim <adhamlingithub@gmail.com>
2021-02-21 11:01:33 -0600

    using StreamWriter/StreamReader

--- fileWriteTest/Program.cs
+++ fileWriteTest/Program.cs
@@ -6,7 +6,10 @@
 * OS          : Windows 10, Visual Studio 2019 community
 * Copyright   : Work based on needed
 *               specifications or project scope
-* Description : write a file -  program will not work with Norton av running because it try's to create a registry key
+* Description : write a file - base program usinf File.WriteAllText 
+*               will not work with Norton av running 
+*               because it try's to create a registry key.
+*               Using StreamWriter instead
 ***************************************************************/
 using System;
 using System.IO;
@@ -35,7 +38,7 @@ namespace fileWriteTest
             // absolute path - anti-virus issue
             // projDir = "C:\\Users\\adhamlin\\Documents\\GitHub\\Csharp_pubs\\CsharpFundamentals\\fileWriteTest";
 
-            string phrase = "\r\n\tI never saw a man who looked";  // carrage return, new line, tab
+            string phrase = "\tI never saw a man who looked";  // carrage return, new line, tab
             phrase += "\r\n\twith such a wistful eye";
             phrase += "\r\n\tupon that little text of blue";
             phrase += "\r\n\twhich prisoners call the sky";
@@ -59,6 +62,57 @@ namespace fileWriteTest
                     Console.WriteLine(error.Message);
                 }
             }
+            Console.WriteLine("^^^ Using 'File.WriteAllText' attempt completed ^^^");
+            Console.WriteLine();  //space in output
+            Console.WriteLine();  //space in output
+
+            // ======= USING StreamWriter =======
+            // will overwrite existing file(s)
+            try
+            {
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamWriter sw = new StreamWriter($"{projDir}\\SWTest.txt");
+                // write poem
+                sw.WriteLine(phrase);
+                //Write a line of text
+                sw.WriteLine("\n\nHello World!!");
+                //Write a second line of text
+                sw.WriteLine("~From the StreamWriter class");
+                //Close the file
+                sw.Close();
+                Console.WriteLine($"File written to: {projDir}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            Console.WriteLine("^^^ Using 'StreamWriter' attempt completed ^^^");
+            Console.WriteLine();  //space in output
+
+
+            Console.WriteLine("===== READING FILE =====");
+            try
+            {
+                // ======= USING StreamReader =======
+                // read file using StreamReader
+                // pass path and file name into StreamReader
+                StreamReader sr = new StreamReader($"{projDir}\\SWTest.txt");
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+                    //write the line to console window
+                    Console.WriteLine(line);
+                    //Then read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Exception: " + error.Message);
+            } 
 
             Console.WriteLine("\n\n\n");
         }
