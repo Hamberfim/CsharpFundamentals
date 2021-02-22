40fc778523528f54d0ad4a502faca061a7095f6d
Hamberfim <adhamlingithub@gmail.com>
2021-02-22 14:03:03 -0600

    string array and StreamWriter/Reader

---
+++ stringArrayStreamWriterReader/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace stringArrayStreamWriterReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory();
+            Console.WriteLine(path);
+
+            path = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(path)));
+            Console.WriteLine(path);
+
+            path = $"{path}\\myStringFile.txt";
+            Console.WriteLine(path);
+
+
+
+            Console.WriteLine("\n\n\n");
+        }
+    }
+}
