a58f5d617da73579c5524300b34f5812be60b9b5
Hamberfim <adhamlingithub@gmail.com>
2021-02-22 14:48:25 -0600

    string array StreamWriter/StreamReader

--- stringArrayStreamWriterReader/Program.cs
+++ stringArrayStreamWriterReader/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/***************************************************************
+* Name        : stringArrayStreamWriterReader
+* Author      : Anthony Hamlin
+* Created     : 02/22/2021 
+* Version     : 1.0
+* OS          : Windows 10, Visual Studio 2019 community
+* Copyright   : Work based on needed
+*               specifications or project scope
+*               
+* Description : Write/Append/Read a file - using a string array,
+*               StreamWriter and StreamReader
+***************************************************************/
+using System;
 using System.IO;
 
 namespace stringArrayStreamWriterReader
@@ -8,15 +20,110 @@ namespace stringArrayStreamWriterReader
         static void Main(string[] args)
         {
             string path = Directory.GetCurrentDirectory();
-            Console.WriteLine(path);
-
+            // Console.WriteLine(path);
             path = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(path)));
-            Console.WriteLine(path);
+            // Console.WriteLine(path);
+            path = $"{path}\\DeepThoughtsFile.txt";
+            // Console.WriteLine(path);
 
-            path = $"{path}\\myStringFile.txt";
-            Console.WriteLine(path);
+            string[] deepthought = new string[]
+            {
+                "\tIf you work on a lobster boat,",
+                "\tsneaking up behind someone",
+                "\tand pinching him is probably",
+                "\ta joke that gets old real fast."
+            };
+
+            string attrib = "\r\n\tDeep Thoughts";
+            attrib += "\r\n\t\t~ Jack Handey";
 
 
+            // Write deapthoughts to a file
+            try
+            {
+                using (StreamWriter sw  = new StreamWriter(path))
+                {
+                    foreach(string line in deepthought)
+                    {
+                        sw.WriteLine(line);
+                    }
+                }
+                Console.WriteLine($"Done! File can be found at:\n{path}");
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+
+
+            // read deapthoughts
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    Console.WriteLine("\t===== Reading File =====");
+                    string line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        //write the line to console window
+                        Console.WriteLine(line);
+                        //Then read the next line
+                        line = sr.ReadLine();
+                    }
+                    //close the file
+                    sr.Close();
+
+                }
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+            Console.WriteLine();  //space in output
+
+
+            // append deapthoughts
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    sw.WriteLine(attrib);
+                }
+                Console.WriteLine();  //space in output
+                Console.WriteLine($"File Appended: {path}");
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+            Console.WriteLine();  //space in output
+
+
+            // read Appedned deapthoughts
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    Console.WriteLine("\t===== Reading Appended File =====");
+                    string line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        //write the line to console window
+                        Console.WriteLine(line);
+                        //Then read the next line
+                        line = sr.ReadLine();
+                    }
+                    //close the file
+                    sr.Close();
+
+                }
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
 
             Console.WriteLine("\n\n\n");
         }
