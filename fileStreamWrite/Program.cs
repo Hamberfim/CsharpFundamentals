72b14c9707f5073f4292e196c18c2bf40db2149f
Hamberfim <adhamlingithub@gmail.com>
2021-02-22 12:39:05 -0600

    file reading/writing

---
+++ fileStreamWrite/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+
+namespace fileStreamWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "System.IO StreamWriter, StreamReader";
+            // get current dir
+            string path = Directory.GetCurrentDirectory();
+            // Write to Project directory - up three directories (NOTE: 'Path' is not the same as 'path')
+            string projDir = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(path)));  // each call goes up its parent directory
+            Console.WriteLine($"Project directory is {projDir}");  // write/read file at this directory
+            projDir = $"{projDir}\\SWTest.txt";
+            Console.WriteLine($"Project file path {projDir}");
+            Console.WriteLine();  //space in output
+
+            // text to write
+            string poem = "\tPerfumes bloomed from a brood of dawns";
+            poem += "\r\n\tThat always lies on the straw of the stars,";
+            poem += "\r\n\tAs the day depends on innocence";
+            poem += "\r\n\tThe whole world depends on your pure eyes";
+            poem += "\r\n\tAnd all my blood flows in their looks.";
+
+            string attrib = "\r\n\t\t'La Courbe de tes yeux'";
+            attrib += "\r\n\t\t~by Paul Éluard";
+
+            // ======= write file 
+            try
+            {
+                // write file and close StreamWriter
+                StreamWriter sw = new StreamWriter(projDir);
+                sw.WriteLine(poem);
+                //close the file
+                sw.Close();
+                Console.WriteLine();  //space in output
+                Console.WriteLine($"File written to: {projDir}");
+                
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+            Console.WriteLine();  //space in output
+
+            Console.WriteLine("===== Reading Text FILE =====");
+            Console.WriteLine();  //space in output
+
+            // ======= Read file
+            try
+            {
+                // ======= USING StreamReader =======
+                // read file using StreamReader
+                // pass path/file name into StreamReader
+                StreamReader sr = new StreamReader(projDir);
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+                    //write the line to console window
+                    Console.WriteLine(line);
+                    //Then read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Exception: " + error.Message);
+            }
+            Console.WriteLine();  //space in output
+
+
+            // ======= append file
+            try
+            {
+                // append the same file with 'using' - will auto close
+                using (StreamWriter sa = new StreamWriter(projDir, true))
+                {
+                    sa.WriteLine(attrib);
+                    Console.WriteLine();  //space in output
+                    Console.WriteLine($"File appended: {projDir}");
+                }
+                
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+            Console.WriteLine();  //space in output
+
+            Console.WriteLine("===== APPENDED Reading FILE again =====");
+            Console.WriteLine();  //space in output
+
+            // ======= read file again
+            try
+            {
+                // ======= USING StreamReader =======
+                // read file using StreamReader
+                // pass path/file name into StreamReader
+                StreamReader sr = new StreamReader(projDir);
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+                    //write the line to console window
+                    Console.WriteLine(line);
+                    //Then read the next line
+                    line = sr.ReadLine();
+                }
+                //close the file
+                sr.Close();
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Exception: " + error.Message);
+            }
+
+            Console.WriteLine("\n\n\n");
+        }
+    }
+}
--- fileWriteTest/Program.cs
+++ fileWriteTest/Program.cs
@@ -2,14 +2,14 @@
 * Name        : fileWriteTest
 * Author      : Anthony Hamlin
 * Created     : 02/19/2021
+* Modified    : 02/22/2021
 * Version     : 1.0
 * OS          : Windows 10, Visual Studio 2019 community
 * Copyright   : Work based on needed
 *               specifications or project scope
-* Description : write a file - base program using File.WriteAllText 
-*               will not work with Norton av running 
-*               because it try's to create a registry key.
-*               Using StreamWriter instead
+*               
+* Description : Write a file - using File.WriteAllText()
+*               Append/Then Read a file - using File.AppendAllText(), File.ReadAllText()
 ***************************************************************/
 using System;
 using System.IO;
@@ -20,45 +20,42 @@ namespace fileWriteTest
     {
         static void Main(string[] args)
         {
-            Console.Title = "Write File Test";
+            Console.Title = "Write, Read, Append File Test";
 
-            //string dir_path = Path.Combine(Environment.CurrentDirectory);
-            //Console.WriteLine($"The current Environment Directory is {dir_path}");
-
-            // When ran path is '...\<current project directory>\bin\Debug\netcoreapp3.1'
+            // string dir_path = Path.Combine(Environment.CurrentDirectory);
+            // Console.WriteLine($"The current Environment Directory is {dir_path}");  // When ran path is '...\<current project directory>\bin\Debug\netcoreapp3.1'
             string path = Directory.GetCurrentDirectory();
-            // I wan to use a relative path to the project directory - this directory/control may not be correct if the app was deployed
-            Console.WriteLine($"Current directory is {path}");
-
-            // Write ro Project directory - up three directories (NOTE: 'Path' is not the same as 'path')
-            string projDir = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(path)));  // each call gets its parent directory
-            Console.WriteLine($"Project directory is {projDir}");  // write/read file at this directory
+            // Console.WriteLine($"Current directory is {path}");  // I wan to use a relative path to the project directory - this directory/control may not be correct if the app was deployed
+            // Write to Project directory - up three directories (NOTE: 'Path' is not the same as 'path')
+            string projDir = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(path)));  // each call gets its parent directory - up one level
+            // Console.WriteLine($"Project directory is {projDir}");  // write/read file at this directory
             Console.WriteLine();  //space in output
 
-            // absolute path - anti-virus issue
-            // projDir = "C:\\Users\\adhamlin\\Documents\\GitHub\\Csharp_pubs\\CsharpFundamentals\\fileWriteTest";
+            // Project directory path plus file name
+            projDir = $"{projDir}\\FWTest.txt";
+            Console.WriteLine($"File Write Path is {projDir}");
 
-            string phrase = "\t'The Ballad of Reading Gaol'";  // carrage return, new line, tab
-            phrase += "\r\n\tI never saw a man who looked";
-            phrase += "\r\n\twith such a wistful eye";
-            phrase += "\r\n\tupon that little text of blue";
-            phrase += "\r\n\twhich prisoners call the sky";
-            phrase += "\r\n\tAnd at every drifting cloud that went";
-            phrase += "\r\n\tWith sails of silver by.";
-            phrase += "\r\n\t\t~Oscar Wilde";
+            string phrase1 = "\t'The Ballad of Reading Gaol'";  // carrage return, new line, tab
+            phrase1 += "\r\n\tI never saw a man who looked";
+            phrase1 += "\r\n\twith such a wistful eye";
+            phrase1 += "\r\n\tupon that little text of blue";
+            phrase1 += "\r\n\twhich prisoners call the sky";
+            phrase1 += "\r\n\tAnd at every drifting cloud that went";
+            phrase1 += "\r\n\tWith sails of silver by.";
+            phrase1 += "\r\n\t\t~Oscar Wilde";
 
 
-            /*// check if text file exists - NOT USING - it try's to create a registry key and is blocked by Norton 360 AV
-            if(File.Exists(projDir))
+            // File.WriteAllText - check if text file exists
+            if (File.Exists(projDir))
             {
-                Console.WriteLine($"File Already Exist as: {projDir}");
+                Console.WriteLine($"File Already Exists at {projDir}");
             }
             else
             {
                 // write text file
                 try
                 {
-                    File.WriteAllText(projDir, phrase);
+                    File.WriteAllText(projDir, phrase1);
                     Console.WriteLine($"File written to: {projDir}");
                 }
                 catch (Exception error)
@@ -66,58 +63,92 @@ namespace fileWriteTest
                     Console.WriteLine(error.Message);
                 }
             }
-            Console.WriteLine("^^^ Using 'File.WriteAllText' attempt completed ^^^");
-            Console.WriteLine();  //space in output
-            Console.WriteLine();  //space in output*/
+            Console.WriteLine("^^^ 'File.WriteAllText' attempt completed ^^^");
+            Console.WriteLine();  // space
 
-            // ======= USING StreamWriter =======
-            // will overwrite existing file(s)
-            try
+            // File.ReadAllText - check if text file exists
+            if (File.Exists(projDir))
             {
-                //Pass the filepath and filename to the StreamWriter Constructor
-                StreamWriter sw = new StreamWriter($"{projDir}\\SWTest.txt");
-                // write poem
-                sw.WriteLine(phrase);
-                //Write a line of text
-                sw.WriteLine("\n\nHello World!!");
-                //Write a second line of text
-                sw.WriteLine("~From the StreamWriter class");
-                //Close the file
-                sw.Close();
-                Console.WriteLine($"File written to: {projDir}");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception: " + e.Message);
-            }
-            Console.WriteLine("^^^ Using 'StreamWriter' attempt completed ^^^");
-            Console.WriteLine();  //space in output
-
-
-            Console.WriteLine("===== Reading Text FILE back into Console=====");
-            Console.WriteLine();  //space in output
-            try
-            {
-                // ======= USING StreamReader =======
-                // read file using StreamReader
-                // pass path and file name into StreamReader
-                StreamReader sr = new StreamReader($"{projDir}\\SWTest.txt");
-                string line = sr.ReadLine();
-                while (line != null)
+                try
                 {
-                    //write the line to console window
-                    Console.WriteLine(line);
-                    //Then read the next line
-                    line = sr.ReadLine();
+                    Console.WriteLine("===== Reading FWTest.txt FILE =====");
+                    // one of two ways to read/display output to console (1)
+                    string text = File.ReadAllText(projDir);  // new lines and tabs are preserved when read by ReadAllText()
+                    Console.WriteLine($"File Read: {projDir}\n");
+                    Console.WriteLine($"{text}\n");
+                    Console.WriteLine();  // space
+                    // two of two ways to read/display output to console (2)
+                    string[] lines = File.ReadAllLines(projDir);  // ONLY tab is preserved when read by ReadAllLine()
+                    int num = 1;
+                    foreach(string line in lines)
+                    {
+                        Console.WriteLine($"{num}: {line} ");
+                        num++;
+                    }
                 }
-                //close the file
-                sr.Close();
-
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.Message);
+                }
             }
-            catch (Exception error)
+            else
             {
-                Console.WriteLine("Exception: " + error.Message);
-            } 
+                Console.WriteLine($"File Not Found at {projDir}");
+            }
+            Console.WriteLine();  // space
+
+
+            // File.AppendAllTest - check if text file exists
+            string[] phrase2 = new string[]
+            {
+                "\tln Debtors' Yard the stones are hard",
+                "\tAnd the dripping wall is high"
+            };
+            string attrib = "\r\n\tmore from 'The Ballad of Reading Gaol'";
+            attrib += "\t\t(Oscar Wilde - 1898)";
+            if(File.Exists(projDir))
+            {
+
+                File.AppendAllText(projDir, attrib);
+                Console.WriteLine($"Appended To File at {projDir}");
+            }
+            else
+            {
+                Console.WriteLine($"File Not Found at {projDir}");
+            }
+            Console.WriteLine();  // space
+
+
+            // File.ReadAllText - check if text file exists
+            if (File.Exists(projDir))
+            {
+                try
+                {
+                    Console.WriteLine("===== Reading FWTest.txt FILE AFTER APPEND ====");
+                    // one of two ways to read/display output to console (1)
+                    string text = File.ReadAllText(projDir);  // new lines and tabs are preserved when read by ReadAllText()
+                    Console.WriteLine($"File Read: {projDir}\n");
+                    Console.WriteLine($"{text}\n");
+                    Console.WriteLine();  // space
+                    // two of two ways to read/display output to console (2)
+                    string[] lines = File.ReadAllLines(projDir);  // ONLY tab is preserved when read by ReadAllLine()
+                    int num = 1;
+                    foreach(string line in lines)
+                    {
+                        Console.WriteLine($"{num}: {line} ");
+                        num++;
+                    }
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"File Not Found at {projDir}");
+            }
+            Console.WriteLine();  // space
 
             Console.WriteLine("\n\n\n");
         }
