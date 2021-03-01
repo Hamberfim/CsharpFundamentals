1d0958f6b0b06ede2de2c9dc0243d2c471230aea
Hamberfim <adhamlingithub@gmail.com>
2021-03-01 13:19:08 -0600

    debugging using watch/step-into

---
+++ debugStepInto/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace debugStepInto
+{
+    class Program
+    {
+        static int square(int number)
+        {
+            return number * number;
+        }
+        static void Main(string[] args)
+        {
+            int pass = 0;
+            int unit = 2;
+
+            for (int i = 1; i < 8; i++)
+            {
+                pass = pass + 1;
+                unit = square(unit);
+
+                if (unit == 0)
+                {
+                    Console.WriteLine($"Pass: {pass}, Int Unit squared surpassed data type size limit, int value is {unit}");
+                }
+                else
+                {
+                    Console.WriteLine($"Pass: {pass}, Int Unit squared = {unit}");
+                }
+                
+            }
+
+
+            Console.WriteLine("\n\n");
+        }
+    }
+}
