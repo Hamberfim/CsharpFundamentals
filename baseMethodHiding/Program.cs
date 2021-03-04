0df0a003946063173de59bcf2b33bbf4af7d527b
Hamberfim <adhamlingithub@gmail.com>
2021-03-04 16:50:28 -0600

    hiding base methods

---
+++ baseMethodHiding/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace baseMethodHiding
+{
+    class Program
+    {
+        // base class
+        public class Man
+        {
+
+            // default constructor
+            public void speak()
+            {
+                Console.WriteLine("Hello ");
+            }
+
+            // overloading
+            public void speak(string message)
+            {
+                Console.WriteLine(message + "!\n");
+            }
+
+        }
+
+        // derived class
+        public class Hombre : Man
+        {
+            public new void speak(string message)
+            {
+                Console.WriteLine(message);
+                base.speak();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Man henry = new Man();
+            henry.speak();  // defalut constructor call
+            henry.speak("It's a beautiful day");
+
+            Hombre enrique = new Hombre();
+            enrique.speak("Hola ");
+            ((Man)enrique).speak("Es una tarde hermosa");
+
+
+
+            Console.WriteLine("\n\n");
+        }
+    }
+}
