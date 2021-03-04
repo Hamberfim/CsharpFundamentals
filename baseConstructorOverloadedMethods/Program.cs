8b00d8fb56025a30ce3baadd06e98c20262f4a97
Hamberfim <adhamlingithub@gmail.com>
2021-03-04 10:40:51 -0600

    Calling base constructors

---
+++ baseConstructorOverloadedMethods/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace baseConstructorOverloadedMethods
+{
+    class Program
+    {
+        // base parent class
+        public class Parent
+        {
+            // default constructor
+            public Parent()
+            {
+                Console.WriteLine("Default Parent constructor called");
+            }
+
+            // overloaded constructor
+            public Parent(int num)
+            {
+                Console.WriteLine("\tOverloaded Parent constructor called: " + num);
+            }
+
+        }
+
+        // derived class
+        public class Daughter : Parent
+        {
+            // default constructor
+            public Daughter()
+            {
+                Console.WriteLine("\tDaughter : Parent derived class default constructor Called\n");
+            }
+        }
+
+        // derived class
+        public class Son : Parent
+        {
+            // default constructor
+
+            public Son()
+            {
+                Console.WriteLine("\tSon : Parent derived class default constructor Called\n");
+            }
+
+            // overloaded constructor
+            public Son(int num):base(num)
+            {
+                Console.WriteLine("\tSon : Parent derived class overloaded constructor called: " + num);
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            Parent Anthony = new Parent();
+            Parent Shelly = new Parent(50);
+
+            Daughter Lillie = new Daughter();
+
+            Son Tony = new Son();
+            Son Andrew = new Son(100);
+
+            Console.WriteLine("\n\n\n");
+        }
+    }
+}
