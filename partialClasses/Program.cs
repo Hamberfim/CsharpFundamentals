bb2f29321a1912235e6e964238019dd7d0cc9924
Hamberfim <adhamlingithub@gmail.com>
2021-03-07 10:20:35 -0600

    utilizing partial classes

---
+++ partialClasses/Program.cs
@@ -0,0 +1,87 @@
+﻿/***************************************************************
+* Name        : partialClasses - Program.cs
+* Author      : Anthony Hamlin
+* Created     : 03/07/2021
+* Version     : 1.0
+* OS          : Windows 10, Visual Studio 2019 community
+* Copyright   : Work based on needed
+*               specifications or project scope
+* Description : Employing Partial classes       
+***************************************************************/
+using System;
+
+namespace partialClasses
+{
+    public partial class Sailboat
+    {
+        // decribe() and describeFull() display methods here, constructors in partial Vehicle.cs class
+        // display method
+        public void describe()
+        {
+            Console.WriteLine($"\tMake: {make}\n\tModel: { model}");
+            Console.WriteLine();
+        }
+
+        // display method for overloaded constructor
+        public void describeFull()
+        {
+            Console.WriteLine($"\tMake: {make}\n\tModel: { model}\n\tYear: {year}\n\tPrice: {price:C}");
+            Console.WriteLine();
+        }
+
+    }
+
+    public partial class Motorhome
+    {
+        // decribe() and describeFull() diplay method/constructors in partial Vehicle.cs class
+    }
+
+    public partial class Moped
+    {
+        // decribe() and describeFull() diplay method/constructors in partial Vehicle.cs class
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Partial Classes -  Vehicle Stock";
+
+            Console.WriteLine("===== Sailboat Stock =====");
+            Sailboat laserBoat = new Sailboat("Laser", "Classic");
+            laserBoat.describe();
+            laserBoat = new Sailboat("Laser", "Classic", 2004, 45999);
+            laserBoat.describeFull();
+            Sailboat pocketCruiser = new Sailboat("Beneteau", "First 24-Pocket Cruiser");
+            pocketCruiser.describe();
+            pocketCruiser = new Sailboat("Beneteau", "First 24-Pocket Cruiser", 2019, 88401);
+            pocketCruiser.describeFull();
+
+
+            Console.WriteLine("===== RV Stock =====");
+            Motorhome smallRV = new Motorhome("Moto-Craft", "Tahoe");
+            smallRV.describe();
+            smallRV = new Motorhome("Moto-Craft", "Tahoe", 2021, 89999);
+            smallRV.describeFull();
+            Motorhome wayfarer = new Motorhome("Tiffin", "Wayfarer");
+            wayfarer.describe();
+            wayfarer = new Motorhome("Tiffin", "Wayfarer", 2019, 114995);
+            wayfarer.describeFull();
+
+
+            Console.WriteLine("===== Moped Stock =====");
+            Moped vespa = new Moped("Vespa", "Sei-Giorni");
+            vespa.describe();
+            vespa = new Moped("Vespa", "Sei-Giorni", 2021, 7749);
+            vespa.describeFull();
+            Moped kisbee = new Moped("Peugeot", "Kisbee 50 4T RS");
+            kisbee.describe();
+            kisbee = new Moped("Peugeot", "Kisbee 50 4T RS", 2019, 2538);
+            kisbee.describeFull();
+
+            Console.WriteLine("\n\n");
+
+        }
+    }
+
+}
---
+++ partialClasses/Vehicles.cs
@@ -0,0 +1,123 @@
+﻿/***************************************************************
+* Name        : partialClasses - Vehicles.cs
+* Author      : Anthony Hamlin
+* Created     : 03/07/2021
+* Version     : 1.0
+* OS          : Windows 10, Visual Studio 2019 community
+* Copyright   : Work based on needed
+*               specifications or project scope
+* Description : Employing Partial classes       
+***************************************************************/
+using System;
+
+namespace partialClasses
+{
+    // SailBoat
+    public partial class Sailboat
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+
+        // constructor
+        public Sailboat(string make, string model)
+        {
+            this.make = make;
+            this.model = model;
+
+        }
+
+        // overloading constructor
+        public Sailboat(string make, string model, int year, double price)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.price = price;
+        }
+    }
+
+    // Motorhome
+    public partial class Motorhome
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+
+        // constructor
+        public Motorhome(string make, string model)
+        {
+            this.make = make;
+            this.model = model;
+
+        }
+
+        // overloading constructor
+        public Motorhome(string make, string model, int year, double price)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.price = price;
+
+        }
+
+        // display method
+        public void describe()
+        {
+            Console.WriteLine($"\tMake: {make}\n\tModel: { model}");
+            Console.WriteLine();
+        }
+
+        // display method for overloaded constructor
+        public void describeFull()
+        {
+            Console.WriteLine($"\tMake: {make}\n\tModel: { model}\n\tYear: {year}\n\tPrice: {price:C}");
+            Console.WriteLine();
+        }
+    }
+
+    // Moped
+    public partial class Moped
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+
+        // constructor
+        public Moped(string make, string model)
+        {
+            this.make = make;
+            this.model = model;
+
+        }
+
+        // overloading constructor
+        public Moped(string make, string model, int year, double price)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.price = price;
+
+        }
+
+        // display method
+        public void describe()
+        {
+            Console.WriteLine($"\tMake: {make}\n\tModel: { model}");
+            Console.WriteLine();
+        }
+
+        // display method for overloaded constructor
+        public void describeFull()
+        {
+            Console.WriteLine($"\tMake: {make}\n\tModel: { model}\n\tYear: {year}\n\tPrice: {price:C}");
+            Console.WriteLine();
+        }
+    }
+
+}
