2d1df2f60382194bb31d0b158495e853c10bddf8
Hamberfim <adhamlingithub@gmail.com>
2021-03-11 11:30:07 -0600

    null value types

---
+++ nullableValueTypes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace nullableValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int? userQty;
+            userQty = null;
+
+            double pricePerUnit = 1.99;
+
+            double? totalSale;
+            totalSale = null;
+
+            Console.Write("Enter a quanity as a whole number: ");
+            try
+            {
+                userQty = Convert.ToInt32(Console.ReadLine());
+                if (userQty.HasValue)
+                {
+                    totalSale = pricePerUnit * userQty;
+                    Console.WriteLine($"You have chosen {userQty} widgets @ {pricePerUnit:C2} per item for a total of {totalSale:C2}");
+                }
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine($"{error.Message}  !!! You didn't enter a quantity !!!");
+                userQty = userQty.GetValueOrDefault();
+                totalSale = totalSale.GetValueOrDefault();
+                Console.WriteLine(($"You have chosen {userQty} widgets @ {pricePerUnit:C2} per item for a total of {totalSale:c2}"));
+            }
+
+            Console.WriteLine("\n\n");
+        }
+    }
+}
