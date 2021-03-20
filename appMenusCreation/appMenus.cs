f7cff738fb13d356285f198d69e91cc7f1950d05
Hamberfim <adhamlingithub@gmail.com>
2021-03-20 12:41:13 -0500

    Simple app menu functions

---
+++ appMenusCreation/appMenus.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appMenusCreation
+{
+    public partial class appMenus : Form
+    {
+        public appMenus()
+        {
+            InitializeComponent();
+        }
+
+        private void newFile()
+        {
+            richTextBox1.Text = null;
+            toolStripStatusLabel1.Text = "Ready";
+        }
+
+        private void openFile()
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void saveFile()
+        {
+            saveFileDialog1.Filter = "Text Files | *.txt";
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SaveFile(saveFileDialog1.FileName,RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newFile();
+        }
+
+        private void newToolStripButton_Click(object sender, EventArgs e)
+        {
+            newFile();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFile();
+        }
+
+        private void openToolStripButton_Click(object sender, EventArgs e)
+        {
+            openFile();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFile();
+        }
+
+        private void saveToolStripButton_Click(object sender, EventArgs e)
+        {
+            saveFile();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += Clipboard.GetText(TextDataFormat.Text).ToString();
+        }
+
+        private void pasteToolStripButton_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text += Clipboard.GetText(TextDataFormat.Text);
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string title = "My Fake App";
+            string message = "Just a simple app to create functional application menus.";
+            MessageBox.Show(message, title);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+    }
+}
