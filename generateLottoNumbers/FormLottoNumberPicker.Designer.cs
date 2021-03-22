e3d72e2a04cdceb101146131266201e1e201a785
Hamberfim <adhamlingithub@gmail.com>
2021-03-22 12:27:57 -0500

    Lotto Number generator app

---
+++ generateLottoNumbers/FormLottoNumberPicker.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace generateLottoNumbers
+{
+    partial class FormLottoNumberPicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.lblNum3 = new System.Windows.Forms.Label();
+            this.lblNum4 = new System.Windows.Forms.Label();
+            this.lblNum5 = new System.Windows.Forms.Label();
+            this.lblNum6 = new System.Windows.Forms.Label();
+            this.btnPick = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblAppTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum1.Location = new System.Drawing.Point(321, 38);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(42, 16);
+            this.lblNum1.TabIndex = 0;
+            this.lblNum1.Text = "label1";
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum2.Location = new System.Drawing.Point(279, 38);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(42, 16);
+            this.lblNum2.TabIndex = 1;
+            this.lblNum2.Text = "label2";
+            // 
+            // lblNum3
+            // 
+            this.lblNum3.AutoSize = true;
+            this.lblNum3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum3.Location = new System.Drawing.Point(237, 38);
+            this.lblNum3.Name = "lblNum3";
+            this.lblNum3.Size = new System.Drawing.Size(42, 16);
+            this.lblNum3.TabIndex = 2;
+            this.lblNum3.Text = "label3";
+            // 
+            // lblNum4
+            // 
+            this.lblNum4.AutoSize = true;
+            this.lblNum4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum4.Location = new System.Drawing.Point(195, 38);
+            this.lblNum4.Name = "lblNum4";
+            this.lblNum4.Size = new System.Drawing.Size(42, 16);
+            this.lblNum4.TabIndex = 3;
+            this.lblNum4.Text = "label4";
+            // 
+            // lblNum5
+            // 
+            this.lblNum5.AutoSize = true;
+            this.lblNum5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum5.Location = new System.Drawing.Point(153, 38);
+            this.lblNum5.Name = "lblNum5";
+            this.lblNum5.Size = new System.Drawing.Size(42, 16);
+            this.lblNum5.TabIndex = 4;
+            this.lblNum5.Text = "label5";
+            // 
+            // lblNum6
+            // 
+            this.lblNum6.AutoSize = true;
+            this.lblNum6.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum6.Location = new System.Drawing.Point(111, 38);
+            this.lblNum6.Name = "lblNum6";
+            this.lblNum6.Size = new System.Drawing.Size(42, 16);
+            this.lblNum6.TabIndex = 5;
+            this.lblNum6.Text = "label6";
+            // 
+            // btnPick
+            // 
+            this.btnPick.AutoSize = true;
+            this.btnPick.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPick.Location = new System.Drawing.Point(111, 69);
+            this.btnPick.Name = "btnPick";
+            this.btnPick.Size = new System.Drawing.Size(160, 25);
+            this.btnPick.TabIndex = 6;
+            this.btnPick.Text = "Get My Lucky Numbers";
+            this.btnPick.UseVisualStyleBackColor = true;
+            this.btnPick.Click += new System.EventHandler(this.btnPick_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(287, 69);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::generateLottoNumbers.Properties.Resources.lotto;
+            this.pictureBox1.Location = new System.Drawing.Point(9, 9);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(94, 97);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblAppTitle
+            // 
+            this.lblAppTitle.AutoSize = true;
+            this.lblAppTitle.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAppTitle.Location = new System.Drawing.Point(108, 9);
+            this.lblAppTitle.Name = "lblAppTitle";
+            this.lblAppTitle.Size = new System.Drawing.Size(151, 18);
+            this.lblAppTitle.TabIndex = 9;
+            this.lblAppTitle.Text = "Lotto Number Picker";
+            // 
+            // FormLottoNumberPicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 115);
+            this.Controls.Add(this.lblAppTitle);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnPick);
+            this.Controls.Add(this.lblNum6);
+            this.Controls.Add(this.lblNum5);
+            this.Controls.Add(this.lblNum4);
+            this.Controls.Add(this.lblNum3);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.lblNum1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormLottoNumberPicker";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lotto Number Picker";
+            this.Load += new System.EventHandler(this.LottoNumberPicker_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.Label lblNum3;
+        private System.Windows.Forms.Label lblNum4;
+        private System.Windows.Forms.Label lblNum5;
+        private System.Windows.Forms.Label lblNum6;
+        private System.Windows.Forms.Button btnPick;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblAppTitle;
+    }
+}
+
---
+++ generateLottoNumbers/FormLottoNumberPicker.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace generateLottoNumbers
+{
+    public partial class FormLottoNumberPicker : Form
+    {
+        public FormLottoNumberPicker()
+        {
+            InitializeComponent();
+        }
+
+        // clear method
+        private void Clear()
+        {
+            // default start state of number lables
+            lblNum1.Text = "...";
+            lblNum2.Text = "...";
+            lblNum3.Text = "...";
+            lblNum4.Text = "...";
+            lblNum5.Text = "...";
+            lblNum6.Text = "...";
+
+            // set the button state for starting
+            btnPick.Enabled = true;
+            btnReset.Enabled = false;
+        }
+
+        // load form event handler(s)
+        private void LottoNumberPicker_Load(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        // random number picker
+        private void btnPick_Click(object sender, EventArgs e)
+        {
+            // generate random number array and shuffle the array
+            Random rnd = new Random();
+
+            // array
+            int[] seq = new int[59];
+
+            // fill the array
+            for (int i = 1; i < 60; i++)
+            {
+                seq[i - 1] = i;
+            }
+
+            // shuffle the array - randomize algorithm
+            for (int i = 0; i < 59; i++)
+            {
+                int randomize1 = (rnd.Next() % 59);
+                int randomize2 = seq[i];
+                seq[i] = seq[randomize1];
+                seq[randomize1] = randomize2;
+
+            }
+
+            // display the number label - random numbers values 
+            lblNum1.Text = seq[1].ToString();
+            lblNum2.Text = seq[2].ToString();
+            lblNum3.Text = seq[3].ToString();
+            lblNum4.Text = seq[4].ToString();
+            lblNum5.Text = seq[5].ToString();
+            lblNum6.Text = seq[6].ToString();
+
+            // re-set the button states as Finished
+            btnPick.Enabled = false;
+            btnReset.Enabled = true;
+
+        }
+    }
+}
