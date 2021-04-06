df249cf12664a15d0e2823ee6d53024043d5ab98
Hamberfim <adhamlingithub@gmail.com>
2021-04-06 11:26:35 -0500

    started simple MVC app

---
+++ FutureValue/Models/FutureValueModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureValue.Models
+{
+    public class FutureValueModel  // a model can not have the same name as the namespace
+    {
+        // properties
+        public decimal MonthlyInvestment { get; set; }
+        public decimal YearlyInvestmentRate { get; set; }
+        public int Years { get; set; }
+
+        // method to calculate the future value of the investment input
+        public decimal CalculateFutureValue()
+        {
+            decimal futureValue = 0m;
+            int months = Years * 12;
+            decimal monthlyInterestRate = (YearlyInvestmentRate / 12) / 100;
+            
+            for (int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + MonthlyInvestment) * (1 + monthlyInterestRate);
+            }
+            return futureValue;
+        }
+
+    }
+}
