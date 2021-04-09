b0dbd1c786e3e88fa0efef7849f1387c3d4dfcaf
Hamberfim <adhamlingithub@gmail.com>
2021-04-09 12:45:00 -0500

    test template creation

---
+++ commentHeaderTemplate/Program.cs
@@ -0,0 +1,27 @@
+﻿/***************************************************************
+* Name        : $safeprojectname$
+* Author      : Anthony Hamlin
+* Created     : $time$
+* Version     : 1.0
+* OS          : Windows 10, Visual Studio 2019 community
+* Copyright   : Work based on needed
+*               specifications or project scope
+* Description : This program overall description here
+*               Input:  list and describe
+*               Output: list and describe        
+***************************************************************/
+using System;
+
+namespace commentHeaderTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Project Name: ProjectName");
+            Console.WriteLine("Author: AuthorName");
+            Console.WriteLine("Created: DateCreated");
+            Console.WriteLine("Description: DescriptionContent");
+        }
+    }
+}
