d28bafefde7226c6c5e72c3adb333b652ac788dc
Hamberfim <adhamlingithub@gmail.com>
2021-04-16 16:04:37 -0500

    challenge

---
+++ arrHelperChallenge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace arrHelperChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Word reversal
+            string pangram = "The quick brown fox jumps over the lazy dog";
+            string[] splitPang = pangram.Split(" ");
+            string[] reversedPang = new string[splitPang.Length];
+
+            for (int i = 0; i < splitPang.Length; i++)
+            {
+                char[] strPangLetterArr = splitPang[i].ToCharArray();
+                Array.Reverse(strPangLetterArr);
+                reversedPang[i] = new string(strPangLetterArr);
+            }
+
+            string newPangram = String.Join(" ", reversedPang);
+            Console.WriteLine(newPangram);
+
+            Console.WriteLine("\n\n");
+        }
+    }
+}
