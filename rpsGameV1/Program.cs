00b8e29a51cbec35f6dd13a357773070b802d22e
Hamberfim <adhamlingithub@gmail.com>
2021-05-06 13:37:40 -0500

    rps

---
+++ rpsGameV1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace rpsGameV1
+{
+    class Program
+    {
+        // tuple pattern matching - expression-bodied
+        public static string RockPaperScissorsSwitch(string playerOne, string playerTwo) =>
+            (playerOne, playerTwo) switch
+            {
+                ("rock", "paper") => "Paper Wins, rock covered by paper.",  // +1 player two
+                ("rock", "scissors") => "Rock Wins, rock breaks scissors.", // +1 player one
+                ("paper", "rock") => "Paper Wins, paper covers rock.",  // +1 player one
+                ("paper", "scissors") => "Scissors Wins, paper is cut by scissors.",  // +1 player two
+                ("scissors", "rock") => "Rock Wins, scissors are broken by rock.",  // +1 player two
+                ("scissors", "paper") => "Scissors wins, scissors cut paper.",  // +1 player one
+                (_, _) => "It's a tie!"  // no score
+            };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RockPaperScissorsSwitch("scissors", "rock"));
+            Console.WriteLine(RockPaperScissorsSwitch("paper", "paper"));
+            Console.WriteLine(RockPaperScissorsSwitch("rock", "paper"));
+            Console.WriteLine(RockPaperScissorsSwitch("paper", "scissors"));
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("\n\n");
+        }
+
+    }
+}
