c9328c635b318941d11883167e9de86dcfd1bade
Hamberfim <adhamlingithub@gmail.com>
2021-05-13 12:48:50 -0500

    dry weight converter cups/grams

--- dryWeightConversion/DryWeightConversion.cs
+++ dryWeightConversion/DryWeightConversion.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/***************************************************************
+* Name        : dryWeightConversion
+* Author      : Anthony Hamlin
+* Created     : 05/12/2021
+* Version     : 1.0
+* OS          : Windows 10, Visual Studio 2019 community
+* Copyright   : Work based on needed
+*               specifications or project scope
+* Description : Converts dry cup/gram weights        
+***************************************************************/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
--- dryWeightConversion/Program.cs
+++ dryWeightConversion/Program.cs
@@ -1,3 +1,13 @@
+/***************************************************************
+* Name        : dryWeightConversion
+* Author      : Anthony Hamlin
+* Created     : 05/12/2021
+* Version     : 1.0
+* OS          : Windows 10, Visual Studio 2019 community
+* Copyright   : Work based on needed
+*               specifications or project scope
+* Description : Converts dry cup/gram weights        
+***************************************************************/
 using System;
 using System.Collections.Generic;
 using System.Linq;
