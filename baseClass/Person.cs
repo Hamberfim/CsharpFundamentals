549de395248337a9c4a4655ce2c3fb92300d89c9
Hamberfim <adhamlingithub@gmail.com>
2021-05-14 12:21:24 -0500

    seperate class called from main

---
+++ baseClass/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baseClass
+{
+    public class Person
+    {
+        // constructor - redundant
+        public Person()
+        {
+            // default construct is automatic
+        }
+
+        // getters/setters
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public int Age { get; set; }
+
+        // a field rather than a property
+        string _middleName = "Judas";
+
+        // method - concatinate fName, lName and age.
+        public string GetPersonInfo()
+        {
+            return FName + " " + LName + ", age:" + Age.ToString();
+        }
+
+        // property to mimic part of the method
+        public string FullName
+        {
+            get
+            {
+                return FName + " " + _middleName + " " + LName;
+            }
+        }
+
+    }
+}
---
+++ baseClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace baseClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create the Person object
+            Person billy = new Person();
+            // properties of billy
+            billy.FName = "William";
+            billy.LName = "Tell";
+            billy.Age = 35;
+
+            // call the method that concatenates the person info
+            Console.WriteLine(billy.GetPersonInfo());
+
+            // call the property that mimics part of the method
+            Console.WriteLine(billy.FullName);
+
+
+            Console.WriteLine("\n\n");
+        }
+    }
+}
