2766a844bd4e2ef715499e492745956750cebcb2
Hamberfim <adhamlingithub@gmail.com>
2021-05-19 16:58:22 -0500

    enum

---
+++ enumerationsReview/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace enumerationsReview
+{
+    class Program
+    {
+        enum Language { PL, DE, EN };
+        enum Season { Spring, Summer, Autumn, Winter };
+        enum DayOfWeek { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday};
+
+        static void Main(string[] args)
+        {
+            Language lang = Language.EN;
+            switch (lang)
+            {
+                case Language.PL: 
+                    Console.WriteLine("Polish language chosen");
+                    break;
+                case Language.EN:
+                    Console.WriteLine("English language chosen");
+                    break;
+                default:
+                    Console.WriteLine("German language chosen");
+                    break;
+            }
+
+            Season fall = Season.Autumn;
+            Console.WriteLine($"I love {fall} in Iowa.");
+
+            DayOfWeek thirdDay = DayOfWeek.Tuesday;
+            Console.WriteLine($"It happened on a {thirdDay}.");
+
+
+            Console.WriteLine("\n\n");
+        }
+    }
+}
