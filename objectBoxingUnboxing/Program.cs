3fcf23646644ca6179a8fdb4065e0a9e7c6ad3b3
Hamberfim <adhamlingithub@gmail.com>
2021-05-20 14:11:49 -0500

    object boxing/unboxing

---
+++ objectBoxingUnboxing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace objectBoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal PI = 3.14m;
+            object boxedPi = PI;  // boxing
+            Console.WriteLine($"Decimal Data Type: {PI} HashCode: {PI.GetHashCode()}");
+            Console.WriteLine($"Reference Object Data Type: {boxedPi} HashCode: {boxedPi.GetHashCode()}");
+            PI = 3.140001m;
+            Console.WriteLine();
+            Console.WriteLine($"Change decimal pi value to {PI}");
+            Console.WriteLine($"Decimal Data Type: {PI} HashCode: {PI.GetHashCode()} <= new stack mem");
+            Console.WriteLine($"Reference Object Data Type: {boxedPi} HashCode: {boxedPi.GetHashCode()} <= original ref");
+            decimal PiUnboxed = (decimal)boxedPi;
+            Console.WriteLine($"BoxedPi: {boxedPi} HashCode: {boxedPi.GetHashCode()}, PiUnboxed: {PiUnboxed} HashCode: {PiUnboxed.GetHashCode()}");
+
+            Console.WriteLine("\n\n");
+        }
+    }
+}
